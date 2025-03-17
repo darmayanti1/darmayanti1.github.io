79ac92953affd0ccef9b3af191ee23f290564b7c
darmayanti1 <darmayanti8665@gmail.com>
2025-03-17 10:19:31 +0800

    penambahan java untuk use case

---
+++ cv pribadi/script.js
@@ -0,0 +1,7 @@
+console.log("CV Darma loaded successfully!");
+
+// Fungsi untuk menampilkan kontak
+function showContact() {
+    let contactInfo = document.getElementById("contact-info");
+    contactInfo.style.display = contactInfo.style.display === "none" ? "block" : "none";
+}
