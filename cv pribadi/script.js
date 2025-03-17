216155eba99a8987aba3b94deb89040b2a83a32c
darmayanti1 <darmayanti8665@gmail.com>
2025-03-17 10:59:37 +0800

    delete js

--- cv pribadi/script.js
+++
@@ -1,7 +0,0 @@
-console.log("CV Darma loaded successfully!");
-
-// Fungsi untuk menampilkan kontak
-function showContact() {
-    let contactInfo = document.getElementById("contact-info");
-    contactInfo.style.display = contactInfo.style.display === "none" ? "block" : "none";
-}
